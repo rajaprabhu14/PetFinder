8a33f038a1eaa83aa6163e8563738d56b08d754e
prabhurajaprabhu <40233019+prabhurajaprabhu@users.noreply.github.com>
2025-03-21 13:22:27 +0530

    Added validation message on image

--- src/app/models/image.ts
+++ src/app/models/image.ts
@@ -1,4 +1,14 @@
 export interface Image {
-    id: string,
-    isImageUploaded: boolean
+  id: string;
+  isImageUploaded: boolean;
+}
+
+export class PetImage implements Image {
+  id: string;
+  isImageUploaded: boolean;
+
+  constructor(id: string, isImageUploaded: boolean) {
+    this.id = id;
+    this.isImageUploaded = isImageUploaded;
+  }
 }
--- src/app/newpet/newpet.component.ts
+++ src/app/newpet/newpet.component.ts
@@ -10,16 +10,15 @@ import {
 import { Pet, PetDetails } from '../models/petDetails';
 import { CommonModule } from '@angular/common';
 import { BehaviorSubject } from 'rxjs';
-import { Image } from '../models/image';
+import { Image, PetImage } from '../models/image';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'newpet',
   standalone: true,
   imports: [FormsModule, ReactiveFormsModule, CommonModule],
-  templateUrl: './newpet.component.html'
+  templateUrl: './newpet.component.html',
 })
-  
 export class NewPetComponent implements OnInit {
   @ViewChild('file') fileInput: any;
 
@@ -34,7 +33,9 @@ export class NewPetComponent implements OnInit {
     private petService: PetService,
     private formBuilder: FormBuilder,
     private router: Router
-  ) {}
+  ) {
+    this.petImage = new PetImage('', false);
+  }
 
   ngOnInit(): void {
     this.initializeForm();
@@ -42,6 +43,11 @@ export class NewPetComponent implements OnInit {
 
   onSave(): void {
     this.formSubmitted = true;
+
+    if (!this.petImage.isImageUploaded) {
+      this.addPetForm.get('filePath')?.setErrors({ missingImage: true });
+    }
+
     if (this.addPetForm.valid) {
       const request = this.addPetForm.value;
       const id = this.petImage.id.split('.')[0].slice(2, 12);
@@ -55,7 +61,7 @@ export class NewPetComponent implements OnInit {
       );
 
       this.petService.addPet(this.petDetails).subscribe((response) => {
-        this.router.navigate([("./pets")]);
+        this.router.navigate(['./pets']);
         console.log(response);
       });
     }
@@ -63,6 +69,7 @@ export class NewPetComponent implements OnInit {
 
   onSaveImage(): void {
     this.formSubmitted = true;
+    this.addPetForm.get("filePath")?.clearAsyncValidators();
     if (this.addPetForm.valid) {
       this.petService.uploadImage(this.file).subscribe((response) => {
         this.petImage = response;
